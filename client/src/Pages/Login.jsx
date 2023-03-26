adb29229464811ae506a532055b70de9fbb52787
anshuman-coder <anshurockingboy23@gmail.com>
2023-03-26 20:07:04 +0530

    designing of the login and signup is done

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
-const Login = () => {
+function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="w-full max-w-md">
+        <form className="bg-white shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
+          <h2 className="text-2xl font-bold mb-6">Login</h2>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+            <Link
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              to="/signup"
+            >
+              Create an account!
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
